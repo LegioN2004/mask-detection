a5179c7c7c0ea495bdd04a74a238cdc069f1a874
legion2004 <milindabarua04@gmail.com>
2025-03-11 23:57:35 +0530

    initial commit

---
+++ train_mask_detector.py
@@ -0,0 +1,4 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Dropout
